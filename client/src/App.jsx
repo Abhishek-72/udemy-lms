bc66e0a95e1e7d2716d97b215a15ab135621e690
Abhishek Tripathi <Abhishek-72>
2024-11-15 17:00:53 +0530

    frontend part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/Auth";
 
 function App() {
   return (
-    <>
-      <Button className="bg-red-600">Check</Button>
-    </>
+    <Routes>
+      <Route path="/auth" element={<AuthPage />}></Route>
+    </Routes>
   );
 }
 
