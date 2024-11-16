b47ac2935b9ff3c20080316b6876a4239ea42b26
Abhishek Tripathi <Abhishek-72>
2024-11-16 15:25:22 +0530

    create get course details by id api route

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -28,3 +28,23 @@ exports.getAllCourses = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getCourseDetailsById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const courseDetails = await Course.findById(id);
+    if (!courseDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: courseDetails,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   addNewCourse,
   getAllCourses,
+  getCourseDetailsById,
 } = require("../controllers/courseController.js");
 const router = express.Router();
 
 router.post("/add", addNewCourse);
 router.get("/get", getAllCourses);
+router.get("/get/details/:id", getCourseDetailsById);
 
 module.exports = router;
