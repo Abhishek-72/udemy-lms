f43c351244fe8e68ff364f74372a6790a742fdd0
Abhishek Tripathi <Abhishek-72>
2024-11-20 16:36:47 +0530

    create api to update user name and email

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,17 @@ const User = require("../models/userModel");
 const sendEmail = require("../utils/email.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) {
+      newObj[key] = obj[key];
+    }
+  });
+  return newObj;
+};
+
 exports.forgotPassword = async (req, res, next) => {
   try {
     // 1) Get user based on the provided email
@@ -107,6 +118,30 @@ exports.updatePassword = async (req, res, next) => {
     success: true,
     message: "Password changed Successfully !",
     token,
+  });
+  next();
+};
+
+// Update user doc
+exports.updateMe = async (req, res, next) => {
+  // Create error if user try to post password data
+  if (req.body.password) {
+    return res.status(400).json({
+      success: false,
+      message:
+        "This route is not for password change. Please user /updatePassword route.",
+    });
+  }
+  // Update user document
+  const filteredBody = filterObj(req.body, "userName", "email");
+  const Updateduser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "User updated successfully !",
+    data: Updateduser,
   });
   next();
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,7 @@ const {
   forgotPassword,
   resetPassword,
   updatePassword,
+  updateMe,
 } = require("../controllers/userController.js");
 const { protect } = require("../controllers/authController.js");
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/forgotPassword", protect, forgotPassword);
 router.patch("/resetPassword/:token", protect, resetPassword);
 router.patch("/updatePassword", protect, updatePassword);
+router.patch("/updateMe", protect, updateMe);
 
 module.exports = router;
